ff1382e43a7de32f784f2bccb2dc110e23a2cf21
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-03-08 12:10:42 +0100

    First alert done, easy stuff!

--- RGLAlertView/ViewController.m
+++ RGLAlertView/ViewController.m
@@ -57,13 +57,36 @@
 
 - (IBAction)onFirstButtonPressed:(UIButton *)sender
 {
-    UIView *firstAlert = [[UIView alloc] initWithFrame:CGRectMake(50, self.deviceHeight/3, self.deviceWidth - 100, 200)];
-    firstAlert.backgroundColor = [UIColor whiteColor];
+    UIView *viewBackgroundAlert = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.deviceWidth, self.deviceHeight)];
+    viewBackgroundAlert.backgroundColor = [UIColor blackColor];
+    viewBackgroundAlert.alpha = 0.75;
+    [self.view addSubview:viewBackgroundAlert];
+
+    UIView *firstAlert = [[UIView alloc] initWithFrame:CGRectMake(50, self.deviceHeight/3 + 30, self.deviceWidth - 100, 150)];
+    firstAlert.backgroundColor = [UIColor colorWithRed:0.73 green:0.27 blue:0.46 alpha:1];
     firstAlert.layer.cornerRadius = 7;
-    firstAlert.transform = CGAffineTransformTranslate(CGAffineTransformMakeRotation(M_PI/6), 0, -500);
+    firstAlert.clipsToBounds = YES;
     [self.view addSubview:firstAlert];
 
-    [UIView animateWithDuration:1 delay:0 usingSpringWithDamping:500 initialSpringVelocity:10 options:0 animations:^{
+    UILabel *labelBodyAlert = [[UILabel alloc] initWithFrame:CGRectMake(15, 0, firstAlert.frame.size.width - 30, firstAlert.frame.size.height - 50)];
+    labelBodyAlert.text = @"This is the very first and most simple alert";
+    labelBodyAlert.textAlignment = NSTextAlignmentCenter;
+    labelBodyAlert.numberOfLines = 10;
+    [labelBodyAlert sizeToFit];
+    CGRect labelRect = labelBodyAlert.frame;
+    firstAlert.frame = CGRectMake(50, (self.deviceHeight - (labelRect.size.height + 50 + 50))/2, self.deviceWidth - 100, labelRect.size.height + 50 + 50);
+    labelBodyAlert.frame = CGRectMake(15, (firstAlert.frame.size.height -  labelRect.size.height - 50)/2, labelRect.size.width, labelRect.size.height);
+    [firstAlert addSubview:labelBodyAlert];
+
+    UIButton *buttonDismissAlert = [[UIButton alloc] initWithFrame:CGRectMake(0, firstAlert.frame.size.height - 50, firstAlert.frame.size.width, 50)];
+    buttonDismissAlert.backgroundColor = [UIColor blackColor];
+    [buttonDismissAlert setTitle:@"Dismiss" forState:UIControlStateNormal];
+    buttonDismissAlert.alpha = 0.3;
+    [firstAlert addSubview:buttonDismissAlert];
+
+    firstAlert.transform = CGAffineTransformTranslate(CGAffineTransformMakeRotation(0), 50, -500);
+
+    [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:10 initialSpringVelocity:20 options:0 animations:^{
         firstAlert.transform = CGAffineTransformTranslate(CGAffineTransformMakeRotation(0), 0, 0);
     } completion:^(BOOL finished) {
 
